ab8d0a4d6b2fbf9c34925c1fa5db1208c50e64e4
Syang3 <Sheldon.Yang@activenetwork.com>
2017-04-18 00:44:58 +0800

    chore: change qaId

--- home/src/app/main.js
+++ home/src/app/main.js
@@ -5,7 +5,7 @@ import compression from 'compression'
 import cors from 'cors'
 
 import conf from '../conf/conf'
-import render from './render'
+// import render from './render'
 
 const log = debug('express')
 const app = express()
@@ -14,16 +14,17 @@ const { port } = conf
 
 app.use(cors())
 app.use(compression())
-app.use('/static/', express.static(path.resolve(__dirname, '../public/static')))
+// app.use('/static/', express.static(path.resolve(__dirname, '../public/static')))
+app.use('/', express.static(path.resolve(__dirname, '../public')))
 
-app.get('/', (req, res) => {
-  res.send(
-    render({
-      title: '风云网络官网',
-      location: req.url,
-    }),
-  )
-})
+// app.get('/', (req, res) => {
+//   res.send(
+//     render({
+//       title: '风云网络官网',
+//       location: req.url,
+//     }),
+//   )
+// })
 
 app.listen(port, () => {
   log(`App is listening port ${port}`)
