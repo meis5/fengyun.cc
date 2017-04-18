befd36ce2b0c417c96a272544bfbbb326a0b770b
Syang3 <Sheldon.Yang@activenetwork.com>
2017-04-18 23:35:31 +0800

    chore: add input disabled state

--- ttd2/src/app/templates/homeTpl.js
+++ ttd2/src/app/templates/homeTpl.js
@@ -8,7 +8,7 @@ export default `
 <head>
   <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 
-  <title>Title</title>
+  <title>天天大贰</title>
   <style>
     body {
       margin: 0;
